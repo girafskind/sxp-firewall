4c791abcb3c9d3edf091b631cc70053bd352b1b4
girafskind <bmo@girafskind.dk>
2020-03-06 17:50:51 +0100

    added find access-list object id by sgt name

--- config.py
+++ config.py
@@ -2,5 +2,5 @@
 
 # Get token from https://{firewall ip}:{port}/doc/ <- remember the last '/'
 
-TOKEN = "695871@4096@1E60@D527C47C835BA781593C7B1AD4D742BBC09E06D8"
+TOKEN = "307775@4096@2882@3F14A22D59BA506344087047ADA07E5DF3BBDC2F"
 SERVER = "https://hawk-asa01:4443"
--- sxp-admin.py
+++ sxp-admin.py
@@ -10,54 +10,62 @@ from config import TOKEN, SERVER
 urllib3.disable_warnings(InsecureRequestWarning)  # disable insecure https warnings
 
 
-req = urllib3.PoolManager()
+# find_obj_in_acl find object id in access-list containing SGT
+def find_acl_sgt(sgt, server, interface):
+	return_data = []
+	raw_data = get_acls(server, interface)
+	data = raw_data['items']
+	for i in data:
+		if 'srcSecurity' in i or 'dstSecurity' in i:
+			if sgt == (i['srcSecurity']['value'] or i['srcSecurity']['value']):
+				return_data.append(i['objectId'])
+
+	return return_data
 
 
-
-def getACLS(server,interface, object=""):
-
-	req = urllib3.PoolManager()
-
-	header = {'content-type': 'application/json','User-agent': 'REST API Agent','X-Auth-Token': TOKEN}
-
-	url = server + "/api/access/in/"+interface+"/rules/"+object
-
+# get_acls function returns access-lists on a interface. ObjectID is optional to get one specific ACL."""
+def get_acls(server, interface, object_id=""):
+	header = {'content-type': 'application/json', 'User-agent': 'REST API Agent', 'X-Auth-Token': TOKEN}
+	url = server + "/api/access/in/" + interface + "/rules/" + object_id
 	response = requests.get(url, headers=header, verify=False)
-	response_json = response.json()
+	json_data = response.json()
+	#print(json.dumps(json_data, indent=2))
+	return json_data
 
-	print("Log1")
-	print(response_json)
 
-
-def enableACL(server,object,interface):
+# Enables a line in the access-list, given obecjtId and interfacename.
+def enable_acl(server, object_id, interface):
 	req = urllib3.PoolManager()
-	header = {'content-type': 'application/json','User-agent': 'REST API Agent','X-Auth-Token': TOKEN}
+	header = {'content-type': 'application/json', 'User-agent': 'REST API Agent', 'X-Auth-Token': TOKEN}
 
 	payload = {"active" : True}
 
-	url = server + "/api/access/in/"+interface+"/rules/"+object
+	url = server + "/api/access/in/" + interface + "/rules/" + object_id
 
 	requests.patch(url, headers=header, data=json.dumps(payload), verify=False)
 
-def disableACL(server,object,interface):
+
+# Disables a line in the access-list, given objectId and interface name.
+def disable_acl(server, object_id, interface):
 	req = urllib3.PoolManager()
-	header = {'content-type': 'application/json','User-agent': 'REST API Agent','X-Auth-Token': TOKEN}
+	header = {'content-type': 'application/json', 'User-agent': 'REST API Agent', 'X-Auth-Token': TOKEN}
+	print(object_id)
+	payload = {"active": False}
 
-	payload = {"active" : False}
-
-	url = server + "/api/access/in/"+interface+"/rules/"+object
+	url = server + "/api/access/in/" + interface + "/rules/" + object_id
 
 	requests.patch(url, headers=header, data=json.dumps(payload), verify=False)
-
-
 
 def main():
-	print("log")
+	print("Starting")
 	#getACLS(SERVER,"LAN","2605530362")
 	#enableACL(SERVER,"2605530362","LAN")
 	#disableACL(SERVER,"2605530362","LAN")
 	#getACLS(SERVER,"LAN","2605530362")
-	getACLS(SERVER,"LAN","3123348233")
+	#get_acls(SERVER, "LAN")
+	list = find_acl_sgt("Bo", SERVER, "LAN")
+	for i in list:
+		disable_acl(SERVER, i, "LAN")
 
 
 if __name__ == '__main__':
